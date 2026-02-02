ece620c9cc4e340184f53683c8819ffeeccebd9b
ben773293-star <ben773293@gmail.com>
2026-02-02 23:32:09 +0000

    Add files via upload

--- reports/urls.py
+++ reports/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), # Ou le nom de ta vue principale
+    path('', views.dashboard, name='index'), # Ou le nom de ta vue principale
 ]

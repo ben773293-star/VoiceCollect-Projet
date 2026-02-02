5f855778ac7a0778e782b7845c9b4a809018f5bf
ben773293-star <ben773293@gmail.com>
2026-02-02 22:52:03 +0000

    Add files via upload

--- reports/urls.py
+++ reports/urls.py
@@ -1,9 +1,7 @@
+# Dans reports/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('supervisor/', views.supervisor_dashboard, name='supervisor_dashboard'),
-    path('export-pdf/', views.export_pdf, name='export_pdf'),
-    path('delete/<int:report_id>/', views.delete_report, name='delete_report'),
+    path('', views.index, name='index'), # Ou le nom de ta vue principale
 ]
